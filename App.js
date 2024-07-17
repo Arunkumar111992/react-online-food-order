495e7404b94585bd956a842603da498131336926
ARUN <earunkumar150@gmail.com>
2024-07-17 12:18:31 +0530

    example 1

--- App.js
+++ App.js
@@ -1,5 +1,30 @@
-const heading = React.createElement(
-    "h1", { id: "heading"}, "Hello React"
+/*
+ <div id="parent1">
+    <div id="child1">
+        <h1>I'm h1 tag</h1>
+        <h2>I'm h2 tag</h2>
+    </div>
+    <div id="child2">
+        <h1>I'm h1 tag</h1>
+        <h2>I'm h2 tag</h2>
+    </div>
+ </div>
+*/
+const heading = React.createElement("div", { id: "parent1"},
+    [
+       React.createElement("div", { id: "child1"},
+       [
+           React.createElement("h1", {}, "I'm h1 tag"), 
+           React.createElement("h2", {}, "I'm h2 tag")
+       ]
+    )],
+    [
+       React.createElement("div", { id: "child2"},
+       [
+           React.createElement("h1", {}, "I'm h1 tag"), 
+           React.createElement("h2", {}, "I'm h2 tag")
+       ]
+    )]
 );
 
 console.log(heading); // object
