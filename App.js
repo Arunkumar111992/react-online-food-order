ff20038bc2f3cc62b5546612ff6bb822bc3b6a2d
ARUN <earunkumar150@gmail.com>
2024-07-18 18:26:30 +0530

    example program

--- App.js
+++ App.js
@@ -1,32 +1,25 @@
-/*
- <div id="parent1">
-    <div id="child1">
-        <h1>I'm h1 tag</h1>
-        <h2>I'm h2 tag</h2>
-    </div>
-    <div id="child2">
-        <h1>I'm h1 tag</h1>
-        <h2>I'm h2 tag</h2>
-    </div>
- </div>
+import React from "react";
+import ReactDOM from "react-dom";
+
+/*React Element
+const jsxheading = (
+    <h1 id="heading">React Element using JSX</h1>
+);
+console.log(jsxheading); // object
 */
-const heading = React.createElement("div", { id: "parent1"},
-    [
-       React.createElement("div", { id: "child1"},
-       [
-           React.createElement("h1", {}, "I'm h1 tag"), 
-           React.createElement("h2", {}, "I'm h2 tag")
-       ]
-    )],
-    [
-       React.createElement("div", { id: "child2"},
-       [
-           React.createElement("h1", {}, "I'm h1 tag"), 
-           React.createElement("h2", {}, "I'm h2 tag")
-       ]
-    )]
+
+const Title = () => (
+    <h1 id="heading">React Title Component</h1>
 );
 
-console.log(heading); // object
+//React Functional Component
+const HeaderComponent = () => (
+    <div id="container">
+        <Title />
+        <h1 id="hading">React Funtional Component</h1>
+    </div>
+)
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+
+root.render(<HeaderComponent/>);
